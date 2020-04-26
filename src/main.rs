ead9d10caf0c7a6e3d0eee8ea68055a555ebdd5b
Newton Ni <nwtnni@gmail.com>
2020-04-25 22:14:58 -0400

    Refactor into structs

--- src/main.rs
+++ src/main.rs
@@ -22,60 +22,33 @@ const COMPACTION_THRESHOLD: u64 = 8 * 1024;
 
 fn main() -> io::Result<()> {
 
-    let mut history = load_history()?;
+    let mut history = History::load()?;
 
     match env::args().nth(1) {
     | Some(path) => {
-        if let Ok(path) = path::Path::new(&path).canonicalize() {
-            if path.is_dir() {
-                let path = path.as_os_str().as_bytes();
-                history.write_all(path)?;
-                history.write_u16::<LittleEndian>(path.len() as u16)?;
-                history.flush()?;
-            }
+        match path::Path::new(&path).canonicalize() {
+        | Ok(path) if path.is_dir() => {
+            history.insert(path.as_os_str().as_bytes())?;
+            history.flush()?;
+        }
+        | _ => (),
         }
     }
     | None => {
         let mut paths = IndexSet::with_hasher(FxBuildHasher::default());
-        let mut buf = Vec::new();
-        let mut pos = history
-            .seek(io::SeekFrom::End(-2))
-            .unwrap_or(0);
+        let mut entries = history.iter_mut();
 
         // Scan backward through the log
-        while pos > 0 && paths.len() < CACHE_SIZE {
+        while let Some(entry) = entries.next()? {
+            match str::from_utf8(&entry) {
+            | Ok(path) if !paths.contains(&*path) => {
+                paths.insert(path.to_owned());
+            }
+            | _ => (),
+            }
 
-            // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
-            //                                    ^
-
-            let len = history.read_u16::<LittleEndian>()?;
-
-            // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
-            //                                              ^
-
-            history.seek(io::SeekFrom::Current(-2 - (len as i64)))?;
-
-            // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
-            //                ^
-
-            buf.clear();
-            buf.resize(len as usize, 0);
-            history.read_exact(&mut buf[..])?;
-
-            // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
-            //                                    ^
-
-            pos = history
-                .seek(io::SeekFrom::Current(-2 -(len as i64)))
-                .unwrap_or(0);
-
-            // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
-            //      ^
-
-            if let Ok(path) = str::from_utf8(&buf) {
-                if !paths.contains(&*path) {
-                    paths.insert(path.to_owned());
-                }
+            if paths.len() > CACHE_SIZE {
+                break;
             }
         }
 
@@ -91,13 +64,10 @@ fn main() -> io::Result<()> {
 
         // If the excess memory usage is above the threshold, then compact
         // the log by rewriting only the relevant entries.
-        if pos > COMPACTION_THRESHOLD {
-            history.seek(io::SeekFrom::Start(0))?;
-            history.set_len(0)?;
-            // Write to log
+        if entries.len() > COMPACTION_THRESHOLD {
+            history.clear()?;
             for path in paths.into_iter().rev() {
-                history.write_all(path.as_bytes())?;
-                history.write_u16::<LittleEndian>(path.len() as u16)?;
+                history.insert(path)?;
             }
             history.flush()?;
         }
@@ -107,19 +77,96 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn load_history() -> io::Result<fs::File> {
-    let mut path = dirs::data_local_dir()
-        .expect("Could not find local data directory");
+struct History {
+    log: fs::File,
+}
 
-    path.push("dvd");
+impl History {
+    pub fn load() -> io::Result<Self> {
+        let mut path = dirs::data_local_dir()
+            .expect("Could not find local data directory");
+        path.push("dvd");
+        fs::create_dir_all(&path)?;
+        path.push("history");
+        fs::OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(&path)
+            .map(|file| History { log: file })
+    }
 
-    fs::create_dir_all(&path)?;
+    pub fn insert<P: AsRef<[u8]>>(&mut self, path: P) -> io::Result<()> {
+        let path = path.as_ref();
+        self.log.write_all(path)?;
+        self.log.write_u16::<LittleEndian>(path.len() as u16)?;
+        Ok(())
+    }
+    
+    pub fn clear(&mut self) -> io::Result<()> {
+        self.log.seek(io::SeekFrom::Start(0))?;
+        self.log.set_len(0)?;
+        Ok(())
+    }
 
-    path.push("history");
+    pub fn flush(&mut self) -> io::Result<()> {
+        self.log.flush()
+    }
 
-    fs::OpenOptions::new()
-        .read(true)
-        .append(true)
-        .create(true)
-        .open(&path)
+    pub fn iter_mut(&mut self) -> HistoryIter<'_> {
+        HistoryIter {
+            buf: Vec::new(),
+            pos: self.log
+                .seek(io::SeekFrom::End(-2))
+                .unwrap_or(0),
+            log: &mut self.log
+        }
+    }
+}
+
+struct HistoryIter<'h> {
+    buf: Vec<u8>,
+    pos: u64,
+    log: &'h mut fs::File,
+}
+
+impl<'h> HistoryIter<'h> {
+    pub fn next(&mut self) -> io::Result<Option<&[u8]>> {
+        if self.pos == 0 {
+            return Ok(None);
+        }
+
+        // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
+        //                                    ^
+
+        let len = self.log.read_u16::<LittleEndian>()?;
+
+        // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
+        //                                              ^
+
+        self.log.seek(io::SeekFrom::Current(-2 - (len as i64)))?;
+
+        // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
+        //                ^
+
+        self.buf.clear();
+        self.buf.resize(len as usize, 0);
+        self.log.read_exact(&mut self.buf[..])?;
+
+        // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
+        //                                    ^
+
+        self.pos = self.log
+            .seek(io::SeekFrom::Current(-2 - (len as i64)))
+            .unwrap_or(0);
+
+        // |   /|0x01|0x00|   /|   b|   a|   r|0x04|0x00|
+        //      ^
+        
+        Ok(Some(&self.buf))
+    }
+
+    pub fn len(&self) -> u64 {
+        self.pos
+    }
 }
